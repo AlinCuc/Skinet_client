57e3d433b2c7d694d219d912e830039b449ede11
AlinCuc <alin.cucc@gmail.com>
2025-03-18 15:42:18 +0200

    Angular Proj

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts,scss}',  // Include fișierele HTML, TS și SCSS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
