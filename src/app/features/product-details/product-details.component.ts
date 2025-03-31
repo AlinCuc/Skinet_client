e763a2e6e0d19b83948254d7744f66bf617af876
AlinCuc <alin.cucc@gmail.com>
2025-03-31 13:52:15 +0300

    added cart services

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -8,6 +8,9 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../core/services/cart.service';
+import { retry } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -19,6 +22,7 @@ import { MatDivider } from '@angular/material/divider';
     MatInputModule,
     MatLabel,
     MatDivider,
+    FormsModule,
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
@@ -26,7 +30,10 @@ import { MatDivider } from '@angular/material/divider';
 export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService);
   private avtivatedRoute = inject(ActivatedRoute);
+  private cartService = inject(CartService);
   product?: Product;
+  qunatityInCart = 0;
+  qunatity = 1;
 
   ngOnInit(): void {
     this.loadProduct();
@@ -35,9 +42,33 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.avtivatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: (product) => (this.product = product),
+      next: (product) => {
+        (this.product = product), this.updateQunatityInCart();
+      },
       error: (error) => console.log(error),
     });
+  }
+  updateCart() {
+    if (!this.product) return;
+    if (this.qunatity > this.qunatityInCart) {
+      const itemToAdd = this.qunatity - this.qunatityInCart;
+      this.qunatityInCart += itemToAdd;
+      this.cartService.addItemToCart(this.product, itemToAdd);
+    } else {
+      const itemToRemove = this.qunatityInCart - this.qunatity;
+      this.qunatityInCart -= itemToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemToRemove);
+    }
+  }
+  updateQunatityInCart() {
+    this.qunatityInCart =
+      this.cartService
+        .cart()
+        ?.items.find((x) => x.productId === this.product?.id)?.quantity || 0;
+    this.qunatity = this.qunatityInCart || 1;
+  }
+  getButtonInText() {
+    return this.qunatityInCart > 0 ? 'UpdateCart' : 'Add to cart';
   }
   preventNegative(event: KeyboardEvent) {
     if (event.key === '-' || event.key === 'e') {
